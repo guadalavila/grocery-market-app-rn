4e28f4a99c85b75ad481e4291a6a80007702f7c7
guadalupe <guadalupe.alavila@gmail.com>
2023-01-11 02:43:33 -0300

    add color

--- src/utils/Colors.ts
+++ src/utils/Colors.ts
@@ -1,8 +1,8 @@
 export const Colors = {
     primary: '#68B984',
+    primaryLight: '#CFFDE1',
     secondary: '#3D5656',
     yellow: '#FED049',
     orange: '#FCB753',
-    light: '#CFFDE1',
     white: '#FFFFFF',
 };

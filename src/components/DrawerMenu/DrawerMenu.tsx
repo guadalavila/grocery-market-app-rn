40d82d8c1d272d88d11116950bf1010121e69c03
guadalupe <guadalupe.alavila@gmail.com>
2023-01-29 18:09:37 -0300

    improve menu
    improve menu

--- src/components/DrawerMenu/DrawerMenu.tsx
+++ src/components/DrawerMenu/DrawerMenu.tsx
@@ -7,6 +7,7 @@ import { Strings } from '../../utils/Strings';
 import { Sizes, TextStyle } from '../../utils/GlobalStyles';
 import Box from '../Box/Box';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { MENU_ITEMS } from '../../utils/Constants';
 
 const DrawerMenu = ({ navigation }: any) => {
     return (
@@ -14,33 +15,18 @@ const DrawerMenu = ({ navigation }: any) => {
             <Box mb={40} ph={Sizes.M}>
                 <Text style={TextStyle.h1}>{Strings.welcome}</Text>
             </Box>
-            <TouchableOpacity
-                style={styles.itemMenu}
-                activeOpacity={0.7}
-                onPress={() => navigation.navigate('FavoritesScreen')}>
-                <Box flexDirection={'row'} justifyContent='space-between'>
-                    <Text style={TextStyle.label}>{Strings.favorites.label}</Text>
-                    <Icon name='chevron-forward-outline' size={20} />
-                </Box>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.itemMenu}
-                activeOpacity={0.7}
-                onPress={() => navigation.navigate('AddressScreen')}>
-                <Box flexDirection={'row'} justifyContent='space-between'>
-                    <Text style={TextStyle.label}>{Strings.address.label}</Text>
-                    <Icon name='chevron-forward-outline' size={20} />
-                </Box>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.itemMenu}
-                activeOpacity={0.7}
-                onPress={() => navigation.navigate('OrdersScreen')}>
-                <Box flexDirection={'row'} justifyContent='space-between'>
-                    <Text style={TextStyle.label}>{Strings.orders.label}</Text>
-                    <Icon name='chevron-forward-outline' size={20} />
-                </Box>
-            </TouchableOpacity>
+            {MENU_ITEMS.map(({ label, screen }) => (
+                <TouchableOpacity
+                    key={label}
+                    style={styles.itemMenu}
+                    activeOpacity={0.7}
+                    onPress={() => navigation.navigate(screen)}>
+                    <Box flexDirection={'row'} justifyContent='space-between'>
+                        <Text style={TextStyle.label}>{label}</Text>
+                        <Icon name='chevron-forward-outline' size={20} />
+                    </Box>
+                </TouchableOpacity>
+            ))}
         </DrawerContentScrollView>
     );
 };
--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,3 +1,20 @@
+import { Strings } from './Strings';
+
 export const STORAGE_KEY = {
     FAVORITE: '@favorite',
 };
+
+export const MENU_ITEMS = [
+    {
+        label: Strings.favorites.label,
+        screen: 'FavoritesScreen',
+    },
+    {
+        label: Strings.address.label,
+        screen: 'AddressScreen',
+    },
+    {
+        label: Strings.orders.label,
+        screen: 'OrdersScreen',
+    },
+];
